ed66f7ad77e5a69e612b19ba8e835739764b75ff
Kimmel, Joshua1 <joshua1.kimmel@intel.com>
2025-12-23 18:27:00 -0800

    test1

--- lib/src/protocols/amba5/dti/dti_controller.dart
+++ lib/src/protocols/amba5/dti/dti_controller.dart
@@ -364,7 +364,7 @@ abstract class DtiController extends Module {
     final nextMsgInValid = Logic(name: 'nextMsgInValid');
     final nextMsgIn = Logic(name: 'nextMsgIn', width: _receiver.msg.width);
 
-    final nextGate = _receiver.msgValid & _receiverCanAccept;
+    final nextGate = _receiverCanAccept;
 
     // flop the next message received from the stream interface
     Sequential(sys.clk, reset: ~sys.resetN, [
